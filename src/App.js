4c437c6a867ed045560d41bd1251dda54a16cf35
marceloaaguilar <marcelodalmeida42@gmail.com.br>
2024-03-11 16:38:06 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
+import GradeCurricular from './components/GradeCurricular';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Carousel/>
+      <GradeCurricular/>
     </div>
   );
 }
